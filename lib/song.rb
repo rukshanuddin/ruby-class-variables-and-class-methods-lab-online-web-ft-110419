e3badc181c6abeab8b8c21151f2c9cebf97194c1
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-10 17:01:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,15 @@ class Song
   end
 
   def self.artist_count
-
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash[artist]
+        artist_hash[artist] +=1
+      else
+        artist_hash[artist] = 1
+      end
+    end
+    artist_hash
   end
 
 end
