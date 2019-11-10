25e0e7e4a6cedc3df9ab502343e5781907b57ca1
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-10 16:48:41 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def new
+    @name
+    @artist
+    @genre
+  end
+  def name
+    @name
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.genre_count
+
+  end
+
+  def self.artist_count
+
+  end
+
+end
